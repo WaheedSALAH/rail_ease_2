4080a9e685167e35cc2da02133a0497cb4326c66
waheedSALAH <w55h44@gmail.com>
2024-05-02 17:51:53 +0300

    login is done ✔️

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool _forgetPasswordClicked = false;
+  bool _registerNowClicked = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,16 +48,34 @@ class Login extends StatelessWidget {
               SizedBox(height: 20),
               buildInputField('Password', 'assets/vectors/group_21_x2.svg'),
               SizedBox(height: 20),
-              GestureDetector(
+              InkWell(
                 onTap: () {
-                  // Add action for forget password
+                  setState(() {
+                    _forgetPasswordClicked = !_forgetPasswordClicked;
+                    // Add action for forget password
+                  });
                 },
-                child: Text(
-                  'Forget Password?',
-                  style: GoogleFonts.inika(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 14,
-                    color: Color(0xFFFF0101),
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 300),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: _forgetPasswordClicked
+                            ? Colors.red
+                            : Colors.transparent,
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                  child: Text(
+                    'Forget Password?',
+                    style: GoogleFonts.inika(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 14,
+                      color: _forgetPasswordClicked
+                          ? Colors.red
+                          : Color(0xFFFF0101),
+                    ),
                   ),
                 ),
               ),
@@ -84,38 +110,54 @@ class Login extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  buildSocialLoginButton('assets/vectors/vector_28_x2.svg'),
+                  Text(
+                    'Or Login with',
+                    style: GoogleFonts.inika(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      // Add action for Facebook login
+                    },
+                    child: buildSocialLoginButton(
+                        'assets/vectors/vector_28_x2.svg'),
+                  ),
                   SizedBox(width: 15),
-                  buildSocialLoginButton('assets/vectors/group_123_x2.svg'),
+                  GestureDetector(
+                    onTap: () {
+                      // Add action for Google login
+                    },
+                    child: buildSocialLoginButton(
+                        'assets/vectors/group_123_x2.svg'),
+                  ),
                 ],
               ),
               SizedBox(height: 20),
-              Center(
-                child: GestureDetector(
-                  onTap: () {
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    _registerNowClicked = !_registerNowClicked;
                     // Add action for register now
-                  },
-                  child: RichText(
-                    text: TextSpan(
-                      style: GoogleFonts.poppins(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18,
-                        color: Color(0xFF000000),
-                      ),
-                      children: [
-                        TextSpan(
-                          text: 'Don’t have an account? ',
-                          style: TextStyle(fontWeight: FontWeight.normal),
-                        ),
-                        TextSpan(
-                          text: 'Register Now',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFFFF0000),
-                          ),
-                        ),
-                      ],
-                    ),
+                  });
+                },
+                child: AnimatedDefaultTextStyle(
+                  duration: Duration(milliseconds: 300),
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                    color: _registerNowClicked ? Colors.red : Color(0xFF000000),
+                  ),
+                  child: Text(
+                    'Don’t have an account? Register Now',
                   ),
                 ),
               ),
@@ -168,31 +210,30 @@ class Login extends StatelessWidget {
   }
 
   Widget buildSocialLoginButton(String iconPath) {
-    return GestureDetector(
-      onTap: () {
-        // Add action for social login
-      },
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Color(0x40000000),
-              offset: Offset(0, 4),
-              blurRadius: 2,
-            ),
-          ],
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Color(0x40000000),
+                offset: Offset(0, 4),
+                blurRadius: 2,
+              ),
+            ],
+          ),
+          width: 65,
+          height: 32.7,
+          padding: EdgeInsets.all(5),
+          child: SvgPicture.asset(
+            iconPath,
+            width: 24,
+            height: 25,
+          ),
         ),
-        width: 65,
-        height: 32.7,
-        padding: EdgeInsets.all(5),
-        child: SvgPicture.asset(
-          iconPath,
-          width: 24,
-          height: 25,
-        ),
-      ),
+      ],
     );
   }
 }
